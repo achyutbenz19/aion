a6ce157e876e148b0f82d20caba3c112e7f32f2a
achyutbenz19 <achyut.benz@gmail.com>
2024-06-04 09:05:28 -0500

    first commit

---
+++ aion/aionApp.swift
@@ -0,0 +1,17 @@
+//
+//  aionApp.swift
+//  aion
+//
+//  Created by Achyut Krishna Byanjankar on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct aionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
