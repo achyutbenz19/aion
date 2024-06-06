257d6841a8bf26d30313732234e1f4ac8ad5d75c
achyutbenz19 <achyut.benz@gmail.com>
2024-06-06 09:26:08 -0500

    get links

--- backend/utils/file_manager.py
+++ backend/utils/file_manager.py
@@ -10,8 +10,12 @@ class FileManager:
     
     def create_bucket(self):
         try:
-            self.storage.create_bucket(self.bucket_name)
-            print("Bucket created successfully.")
+            existing_buckets = self.storage.list_buckets()
+            if self.bucket_name not in [bucket['name'] for bucket in existing_buckets]:
+                self.storage.create_bucket(self.bucket_name)
+                print("Bucket created successfully.")
+            else:
+                print("Bucket already exists.")
         except Exception as e:
             print(f"Error creating bucket: {e}")
     
@@ -31,8 +35,18 @@ class FileManager:
             with open(filepath, 'rb') as f:
                 self.storage.from_(self.bucket_name).upload(file=f, path=os.path.basename(filepath), file_options={"content-type": "image/jpg"})
                 print(f"File {filepath} uploaded successfully.")
+            return self.get_signed_url(os.path.basename(filepath))
         except Exception as e:
             print(f"Error uploading file: {e}")
+    
+    def get_signed_url(self, filename):
+        try:
+            response = self.storage.from_(self.bucket_name).create_signed_url(filename, 63072000)  # 2 years
+            url = response['signedURL']
+            return url
+        except Exception as e:
+            print(f"Error retrieving signed URL: {e}")
+            return None        
     
     def get_docs(self, text, metadata):
         docs = [Document(page_content=text, metadata=metadata)] 
