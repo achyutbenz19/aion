ffc388e874c3d51f2db3dbaeabf65ec98ff4394d
achyutbenz19 <achyut.benz@gmail.com>
2024-06-07 14:50:34 -0500

    better prompt

--- backend/utils/chat_llm.py
+++ backend/utils/chat_llm.py
@@ -8,7 +8,7 @@ from langchain_openai.chat_models import ChatOpenAI
 
 class ChatLLM:
     def __init__(self):
-        self.llm = ChatGroq(temperature=0.5, model_name="mixtral-8x7b-32768", groq_api_key=os.environ.get("GROQ_API_KEY"))
+        self.llm = ChatGroq(temperature=0.2, model_name="mixtral-8x7b-32768", groq_api_key=os.environ.get("GROQ_API_KEY"))
         self.store = {}
         self.system_prompt = """
             You are an assistant model with access to several summaries of the user's activity. Your job is to recall the user's past history and serve as a personalized assistant based on the provided information. You will also be given the image URL in the context.
@@ -16,7 +16,7 @@ class ChatLLM:
             Here is the context:
             {context}
 
-            Answer the following based on the context ONLY IF THE QUERY IS RELEVANT TO THE CONTEXT.
+            Answer the following based on the context ONLY IF THE QUERY IS RELEVANT TO THE CONTEXT. Your answer must be natural. Do not start with "Based on the context, I think...".
             {query}
         """
         
